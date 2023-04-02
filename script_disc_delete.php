086020e55e190c7a2b5ff8a4bd50d52058dd2185
Hamdaoui-mathieu <118903443+Hamdaoui-mathieu@users.noreply.github.com>
2023-04-02 21:58:31 +0200

    Update script_disc_delete.php

---
+++ script_disc_delete.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!(isset($_GET['id'])) || intval($_GET['id']) <= 0) goto TrtRedirection;
+
+
+require "db.php";
+$db = connexionBase();
+
+try {
+
+    $requete = $db->prepare("DELETE FROM disc WHERE disc_id = ?");
+    $requete->execute(array($_GET["id"]));
+    $requete->execute();
+    $requete->closeCursor();
+} catch (Exception $e) {
+    echo "Erreur : " . $requete->errorInfo()[2] . "<br>";
+    die("Fin du script (script_disc_modif.php)");
+}
+
+
+TrtRedirection:
+header("Location: disc.php");
+exit;
